5c59537226d834d15a422105e0b7a6b44e9a0749
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-29 14:08:33 -0600

    Roll back to using Ruby Sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,29 @@
 'use strict';
 
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')({
+  rename: {'gulp-ruby-sass': 'sass'}
+});
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 gulp.task('styles', function () {
-  return gulp.src('styleguide/*.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.cached('styleguide'))
-    .pipe($.sass({
-      precision: 10,
-      onError: console.error.bind(console, 'Sass error:')
-    }))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'IE 9', 'IE 8'],
-      cascade: false
-    }))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('styleguide'))
-    .pipe($.size({title: 'styles'}));
+  return $.sass('styleguide/', {
+    loadPath: ['.'],
+    precision: 10,
+    sourcemap: true
+  })
+  .on('error', function(err) {
+    console.error('Error:', err.message);
+  })
+  .pipe($.cached('styleguide'))
+  .pipe($.autoprefixer({
+    browsers: ['last 2 versions', 'IE 9', 'IE 8'],
+    cascade: false
+  }))
+  .pipe($.sourcemaps.write('.'))
+  .pipe(gulp.dest('styleguide'))
+  .pipe($.size({title: 'styles'}));
 });
 
 gulp.task('serve', ['styles'], function() {
